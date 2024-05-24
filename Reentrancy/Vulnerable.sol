ea43beba411b1935c8e8d5888f7230bdcadf79a6
Jaz-3-0 <sebejaz99@gmail.com>
2024-05-24 21:56:03 +0200

    update init deposit && withdraw

--- Reentrancy/Vulnerable.sol
+++ Reentrancy/Vulnerable.sol
@@ -4,5 +4,20 @@ pragma solidity ^0.8.19;
 contract Vulnerable {
 
     // state variable
+    mapping(address => uint256) public balances;
+
+    // deposit funds
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 _amount) public {
+        require(balances[msg.sender] >= _amount, "Insufficient balance);
+
+        (bool success, ) = msg.sender.call{value: _amount}("");
+        require(success, "Transfer failed");
+        balances[msg.sender] -= _amount;
+    }
+
     
 }
