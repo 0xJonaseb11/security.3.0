3e8b5d72c532db667367d0b88f82440a23c7ec41
Jaz-3-0 <sebejaz99@gmail.com>
2024-05-27 21:57:16 +0200

    update init secure access

--- Access_Control/SecureAccess.sol
+++ Access_Control/SecureAccess.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import 
+/**
+* @dev use @openzeppelin ownable contract to achieve acces control
+
+ */
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SecureAccess is Ownable {
+    
+    function changeOwner(address _newOwner) public onlyOwnable {
+        // utilize _transferOwnership function to control ownershipt correctly
+        transferOwnership(_newOwner);
+    }
+}
