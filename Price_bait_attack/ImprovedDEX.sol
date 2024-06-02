4a59ac66b20b9fe819cd5bf45ebf34790f5ae136
Jaz-3-0 <sebejaz99@gmail.com>
2024-06-02 22:58:51 +0200

    update init update setTokenPrice

---
+++ Price_bait_attack/ImprovedDEX.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract ImprovedDEX {
+
+    mapping(address => uint256) public tokenBalances;
+    uint256 public tokenPrice; // Price of token A in terms of B
+
+    event TradeExecuted(address indexed trader, uint256 amountIn, uint256 amountOut);
+
+    // set token price
+    function setTokenPrice(uint256 _price) external {
+        tokenPrice = _price;
+    }
+
+    
+}
